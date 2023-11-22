de449e126a8d7a5e8f367452ee61b9141fbe5899
Suguru Tokuda <suguru.tokuda@gmail.com>
2023-11-22 14:03:43 -0500

    Initial Commit

---
+++ UsersMap/UsersMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsersMapApp.swift
+//  UsersMap
+//
+//  Created by Suguru Tokuda on 11/22/23.
+//
+
+import SwiftUI
+
+@main
+struct UsersMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
